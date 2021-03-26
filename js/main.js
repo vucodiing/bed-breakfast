d6884e19d78785aeb2ddc97555a2796b9674609e
VuCodiing <vucodiing@gmail.com>
2021-03-26 09:46:25 +0700

    fix scroll menu desktop

--- js/main.js
+++ js/main.js
@@ -10,24 +10,30 @@ $(document).ready(function () {
 });
 
 // scroll page
-// $(document).ready(function () {
-//   $(window).scroll(function (event) {
-//     let pos_body = $("html,body").scrollTop();
+$(document).ready(function () {
+  $(window).scroll(function (event) {
+    let pos_body = $("html,body").scrollTop();
 
-//     // change color menu
-//     if (pos_body > 100) {
-//       $(".header__menu").css({
-//         "background-color": "#000",
-//         "box-shadow": "0px 0px 8px #00000052",
-//       });
-//     } else {
-//       $(".header__menu").css({
-//         "background-color": "transparent",
-//         "box-shadow": "none",
-//       });
-//     }
-//   });
-// });
+    // change color menu
+    if (pos_body > 30) {
+      $("#logo-home-1").css("display", "none");
+      $("#logo-home-2").css("display", "block");
+      $(".scroll").css("color", "#000");
+      $(".menu__icon-count").css("background-color", "#c59d5f");
+      $(".header__menu").css("background-color", "#fff");
+      $(".menu__icon svg").css("color", "#000");
+      $(".menu__icon-count span").css("color", "#fff");
+    } else {
+      $("#logo-home-1").css("display", "block");
+      $("#logo-home-2").css("display", "none");
+      $(".scroll").css("color", "#fff");
+      $(".menu__icon-count").css("background-color", "#fff");
+      $(".header__menu").css("background-color", "#ffb400");
+      $(".menu__icon svg").css("color", "#fff");
+      $(".menu__icon-count span").css("color", "#999");
+    }
+  });
+});
 
 // validate
 // for email home page
