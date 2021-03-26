4cfc3505b78a0a540db719da91e7c00b2d203531
VuCodiing <vucodiing@gmail.com>
2021-03-26 17:32:44 +0700

    fix menu mobile

--- js/main.js
+++ js/main.js
@@ -14,10 +14,12 @@ $(document).ready(function () {
   $(window).scroll(function (event) {
     let pos_body = $("html,body").scrollTop();
 
-    // change color menu
+    // change color menu desktop
     if (pos_body > 30) {
       $("#logo-home-1").css("display", "none");
       $("#logo-home-2").css("display", "block");
+      $("#logo-home-1-mobile").css("display", "none");
+      $("#logo-home-2-mobile").css("display", "block");
       $(".scroll").css("color", "#000");
       $(".menu__icon-count").css("background-color", "#c59d5f");
       $(".header__menu").css("background-color", "#fff");
@@ -26,6 +28,8 @@ $(document).ready(function () {
     } else {
       $("#logo-home-1").css("display", "block");
       $("#logo-home-2").css("display", "none");
+      $("#logo-home-1-mobile").css("display", "block");
+      $("#logo-home-2-mobile").css("display", "none");
       $(".scroll").css("color", "#fff");
       $(".menu__icon-count").css("background-color", "#fff");
       $(".header__menu").css("background-color", "#ffb400");
@@ -52,4 +56,30 @@ $(document).ready(function () {
       },
     },
   });
+});
+
+// show menu tablet and mobile
+$(document).ready(function () {
+  $("#btn-mobile").click(function () {
+    if ($(".header__mobile").css("left") == "-2000px") {
+      $(".header__mobile").css("left", "0");
+    } else {
+      $(".header__mobile").css("left", "-2000px");
+    }
+  });
+});
+////////
+
+$(document).ready(function () {
+  $(".menu_1st").click(function () {
+    if ($(this).find("ul:first").is(":hidden")) {
+      $(this).find("ul:first").slideDown(500);
+    } else {
+      $(this).find("ul:first").slideUp(500);
+    }
+  });
+
+  // $(".menu_2nd").click(function () {
+  //   $(this).find("ul:first").slideDown(500);
+  // });
 });
