9c568635343b299ab7e7cfcea7d601eeaff915b6
VuCodiing <vucodiing@gmail.com>
2021-03-29 00:16:12 +0700

    fix header and contact

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// menu toggle desktop
 $(document).ready(function () {
   $(".menu-main li").hover(
     function () {
@@ -9,43 +10,29 @@ $(document).ready(function () {
     }
   );
 });
-
+// menu toggle mobile and tablet
 $(document).ready(function () {
-  $("#span-1").click(
-    function () {
-      $(".span-1").toggle(500);
-    }
-  );
-  $("#span-2").click(
-    function () {
-      $(".span-2").toggle(500);
-    }
-  );
-  $("#span-3").click(
-    function () {
-      $(".span-3").toggle(500);
-    }
-  );
-  $("#span-4").click(
-    function () {
-      $(".span-4").toggle(500);
-    }
-  );
-  $("#span-5").click(
-    function () {
-      $(".span-5").toggle(500);
-    }
-  );
-  $("#span-6").click(
-    function () {
-      $(".span-6").toggle(500);
-    }
-  );
-  $("#span-7").click(
-    function () {
-      $(".span-7").toggle(500);
-    }
-  );
+  $("#span-1").click(function () {
+    $(".span-1").toggle(500);
+  });
+  $("#span-2").click(function () {
+    $(".span-2").toggle(500);
+  });
+  $("#span-3").click(function () {
+    $(".span-3").toggle(500);
+  });
+  $("#span-4").click(function () {
+    $(".span-4").toggle(500);
+  });
+  $("#span-5").click(function () {
+    $(".span-5").toggle(500);
+  });
+  $("#span-6").click(function () {
+    $(".span-6").toggle(500);
+  });
+  $("#span-7").click(function () {
+    $(".span-7").toggle(500);
+  });
 });
 
 // scroll page
@@ -55,9 +42,35 @@ $(document).ready(function () {
 
     // change color menu desktop
     if (pos_body > 30) {
-
+      // change logo
+      $("#logo-home-1").css("display", "none");
+      $("#logo-home-2").css("display", "block");
+      // change color of menu
+      $(".scroll a").css("color", "#000");
+      // change color of header
+      $(".header").css("background-color", "#fff");
+      // change color icon search, shopping, menu contact
+      $(".header__menu-icon svg").css("color", "#000");
+      // change color and background of span
+      $(".shopping-bag-group span").css({
+        "background-color": "#ffb400",
+        color: "#fff",
+      });
     } else {
-
+      //change logo
+      $("#logo-home-1").css("display", "block");
+      $("#logo-home-2").css("display", "none");
+      // change color of menu
+      $(".scroll a").css("color", "#fff");
+      // change color of header
+      $(".header").css("background-color", "#ffb400");
+      // change color icon search, shopping, menu contact
+      $(".header__menu-icon svg").css("color", "#fff");
+      // change color and background of span
+      $(".shopping-bag-group span").css({
+        "background-color": "#fff",
+        color: "#000",
+      });
     }
   });
 });
@@ -65,12 +78,12 @@ $(document).ready(function () {
 // show header contact
 $(document).ready(function () {
   $("#show-contact-menu").click(function () {
-    $(".header__contact").css({ "right": "0", "visibility": "visible" });
-  })
+    $(".header__contact").css({ right: "0", visibility: "visible" });
+  });
   $(".header__contact-close").click(function () {
-    $(".header__contact").css({ "right": "-405px", "visibility": "hidden" });
-  })
-})
+    $(".header__contact").css({ right: "-405px", visibility: "hidden" });
+  });
+});
 
 // validate
 // for email home page
@@ -102,4 +115,3 @@ $(document).ready(function () {
   });
 });
 ////////
-
