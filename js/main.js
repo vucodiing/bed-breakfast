064ff37446f91fc30161f795f9bd9bcb04cee577
VuCodiing <vucodiing@gmail.com>
2021-03-31 17:15:13 +0700

    add About page

--- js/main.js
+++ js/main.js
@@ -189,12 +189,46 @@ $(".bannerSlider").slick({
   arrows: true,
 });
 
+// slick for testimonial
 $(".testimonials-slick").slick({
   infinite: true,
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
   arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+// slide for icon slick
+$(".iconSlick").slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
   responsive: [
